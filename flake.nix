7e8d71a83f4e2870a48091a8abee206dfa21fe3b
snowflake-hd <david.patrick.gonska@gmail.com>
2026-01-17 19:31:32 +0100

    fix: update flake.nix to include flake-utils and enhance package outputs

--- flake.nix
+++ flake.nix
@@ -3,30 +3,48 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=25.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    appName = "nix-style-search";
-  in
-  {
-    packages.${system} = {
-      ${appName} = pkgs.buildGoModule rec {
-        pname = appName;
-        version = "1.1.0";
-        src = ./.;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        appName = "nix-style-search";
+      in
+      {
+        packages = {
+          ${appName} = pkgs.buildGoModule rec {
+            pname = appName;
+            version = "1.1.2";
+            src = ./.;
 
-       vendorHash = "sha256-NICwZgVyJ9q9Eg0b7vU0QJSkyAaK+BW0fPFGZXtDkbk=";
+            subPackages = [ "cmd/nix-style-search" ];
 
-        meta = with pkgs.lib; {
-          description = "A stylish (community-driven) search tool for Nix packages";
-          license = licenses.asl20;
-          maintainers = [ "snowflake-hd" ];
+            vendorHash = "sha256-NICwZgVyJ9q9Eg0b7vU0QJSkyAaK+BW0fPFGZXtDkbk=";
+
+            meta = with pkgs.lib; {
+              description = "A stylish (community-driven) search tool for Nix packages";
+              license = licenses.asl20;
+              maintainers = [ "snowflake-hd" ];
+            };
+          };
+
+          default = self.packages.${system}.${appName};
         };
-      };
 
-      default = self.packages.${system}.${appName};
-    };
-  };
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.${appName}}/bin/${appName}";
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.golangci-lint
+            pkgs.git
+          ];
+        };
+      }
+    );
 }
